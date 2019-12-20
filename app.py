d7e5f61415ee3eb11b6a39bfa03d94f073c2e502
mirko097 <mirko.urso@patronatosvedu.org>
2019-12-20 14:05:21 +0100

    implementato template bootstrap

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    scritta="Ciao mondo!"
-    return render_template("base.html",  testo=scritta)
+def index( ):
+    titolo="Pagina iniziale"
+    testo="Ciao mondo!"
+    bottone="Più info"
+    return render_template("base.html", 
+                titolo=titolo,
+                testo=testo,
+                bottone=bottone)
+
+
 @app.route('/info')
-def info():
-    scritta="Informazioni!"
-    return render_template("base.html",  testo=scritta)
+def info( ):
+    titolo="Pagina info"
+    testo="Informazioni"
+    bottone="Homepage"
+    return render_template("base.html", 
+                titolo=titolo,
+                testo=testo,
+                bottone=bottone)
+
 if __name__ == '__main__':
-    app.run()
+    app.run( )
